6d4df2ad5d9c6ac59f40d7eb55075ecfe811df70
AlexRestart-bo <aofilippovedu@gmail.com>
2025-01-18 09:06:29 -0500

    Add files via upload

---
+++ module_13_3.py
@@ -0,0 +1,24 @@
+import asyncio
+import logging
+from aiogram import Bot, Dispatcher, types
+from aiogram.filters import Command
+from alex_bot_config import TOKEN
+
+bot = Bot(token=TOKEN)
+dp = Dispatcher()
+
+@dp.message(Command('start'))
+async def cmd_start(message: types.Message):
+    await message.answer('Привет! Я бот помогающий твоему здоровью.')
+
+@dp.message()
+async def all_message(message: types.Message):
+    await message.answer('Введите команду /start, чтобы начать общение.')
+
+async def main():
+    await dp.start_polling(bot)
+
+
+if __name__ == "__main__":
+    logging.basicConfig(level=logging.INFO)
+    asyncio.run(main())
